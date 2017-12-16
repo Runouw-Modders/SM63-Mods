135eb796bbb7dff95c73437d007d6e18748a15d5
DoramBaramour <doram1@hotmail.com>
2017-12-16 15:52:11 -0500

    WEb Interface 0.5
    Can now generate valid SM63 Level Code. Hack payload not processing yet.

--- docs/runouw_script.js
+++ docs/runouw_script.js
@@ -1,20 +1,92 @@
-function switchModes (thisMode) {
-    if (thisMode == "automatic") {
-        setCollectionDisplay("tr.automatic", "table-row");
-        setCollectionDisplay("tr.custom", "none");
-    } else {
-        setCollectionDisplay("tr.automatic", "none");
-        setCollectionDisplay("tr.custom", "table-row");
-    }
-}
-function setCollectionDisplay(thisCollection, thisDisplayStatus) {
-    var myCollection = document.querySelectorAll(thisCollection);
-    var myIndex;
-    for (myIndex = 0; myIndex < myCollection.length; myIndex++) {
-        myCollection[myIndex].style.display=thisDisplayStatus;
-    }
-}
+$(function(){
 
-function sayHi(thisName) {
-    alert('Code isn\'t finished if you hadn\'t noticed');
-}
+    function stepNumber(thisNumber, thisStep, thisMax, thisMin) {
+        var newValue;
+        if (thisMax === undefined) {
+            thisMax = 999;
+        }
+        if (thisMin === undefined) {
+            thisMin = 0;
+        }
+        newValue = thisNumber + thisStep;
+        if (newValue < thisMin) {
+            newValue = thisMin;
+        } else if (newValue > thisMax) {
+            newValue = thisMax;
+        }
+        return newValue;
+    }
+    function sanityCheck(thisNumber, thisMax, thisMin) {
+        var newValue;
+        if (thisMax === undefined) {
+            thisMax = 999;
+        }
+        if (thisMin === undefined) {
+            thisMin = 0;
+        }
+        newValue = thisNumber;
+        if (newValue < thisMin) {
+            newValue = thisMin;
+        } else if (newValue > thisMax) {
+            newValue = thisMax;
+        }
+        return newValue;
+    }
+    
+    $("#assemblerMode").change(function(event){
+        if ($("#assemblerMode").val() == "automatic") {
+            $(".automatic").show();
+            $(".custom").hide();
+        } else {
+            $(".automatic").hide();
+            $(".custom").show();
+        }
+    });
+
+    $(".stepValue").keydown(function(event){
+        //if (event.which == 40) {
+            //$(this).val() = "a"
+        //}
+        $("#myOutput").html("Key: " + event.which);
+    });
+    
+    $("#generateLevelCode").click(function(event){
+        var myLevelCode, myLand, myBackground, myHeight, myWidth, myLevelName;
+        myLevelCode = $("#levelWidth").val() + "x" + $("#levelHeight").val() + "~";
+        myLevelName = encodeURI($("#levelName").val()); 
+        if ($("#levelStyle").val() == "grass"){
+            myLand = "2K2M";
+            myBackground = "~1~1~";
+        } else if ($("#levelStyle").val() == "cave"){
+            myLand = "5d5f";
+            myBackground = "~7~5~";
+        } else if ($("#levelStyle").val() == "snow"){
+            myLand = "6K6M";
+            myBackground = "~8~6~";
+        } else if ($("#levelStyle").val() == "desert"){
+            myLand = "8K*2*";
+            myBackground = "~10~10~";
+        }
+        for(i = 0; i < $("#levelWidth").val(); i++) {
+            myLevelCode += "0*" + ($("#levelHeight").val() - 2) + "*" + myLand
+        }
+        
+        myHeight = ($("#levelHeight").val() * 32) - 128;
+        myWidth = ($("#levelWidth").val() * 32) - 128;
+        myLevelCode += "~1,0," + myHeight + ",0,0,Right|"; //Mario
+        myLevelCode += "6," + myWidth + "," + myHeight; // + "|" //Shine Sprite
+        //myLevelCode += "73,48," & (myHeight + 32) & "," & mySignText & ","; //Sign with selected options
+        myLevelCode += myBackground + myLevelName;
+        
+        $("#myOutput").html(myLevelCode);
+    });
+    
+    $("#selectText").click(function(event){
+        $("#myOutput").select();
+    });
+    
+    
+
+
+}); 
+
