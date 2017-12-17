c274b2e49e6e711489110c6129aca40258332307
DoramBaramour <doram1@hotmail.com>
2017-12-16 22:47:21 -0500

    Web Interface 0.7a
    Added current hacks, and controls enough to get them working. Most
    assembly looks good, needs testing.

--- docs/runouw_script.js
+++ docs/runouw_script.js
@@ -8,7 +8,7 @@ $(function(){
         if (thisMin === undefined) {
             thisMin = 0;
         }
-        newValue = thisNumber + thisStep;
+        newValue = Number(thisNumber) + Number(thisStep);
         if (newValue < thisMin) {
             newValue = thisMin;
         } else if (newValue > thisMax) {
@@ -36,8 +36,15 @@ $(function(){
     function myURLEncode(thisString) {
         var finalResult;
         finalResult = encodeURIComponent(thisString);
+        finalResult = finalResult.replace(/[!]/g, "%21");
+        finalResult = finalResult.replace(/[']/g, "%27");
+        finalResult = finalResult.replace(/[(]/g, "%28");
+        finalResult = finalResult.replace(/[)]/g, "%29");
+        finalResult = finalResult.replace(/[*]/g, "%2A");
         finalResult = finalResult.replace(/[,]/g, "%2C");
+        finalResult = finalResult.replace(/[-]/g, "%2D");
         finalResult = finalResult.replace(/[.]/g, "%2E");
+        finalResult = finalResult.replace(/[_]/g, "%5F");
         return finalResult;
     }
     
@@ -45,25 +52,75 @@ $(function(){
         if ($("#assemblerMode").val() == "automatic") {
             $(".automatic").show();
             $(".custom").hide();
+            $("#enableLDE").prop( "checked", true );
         } else {
             $(".automatic").hide();
             $(".custom").show();
+            $("#enableLDE").prop( "checked", false );
         }
     });
+    $("#enableJumpHeight").change(function(event){
+        if ($("#enableJumpHeight").is(":checked")) {
+            $("#enableStringData").prop( "checked", true );
+        } 
+    });
+    $("#enableGravity").change(function(event){
+        if ($("#enableGravity").is(":checked")) {
+            $("#enableStringData").prop( "checked", true );
+        } 
+    });
+    $("#enablePage").change(function(event){
+        if ($("#enablePage").is(":checked")) {
+            $("#enableStringData").prop( "checked", true );
+        } 
+    });
+    
 
-    $(".stepValue").keydown(function(event){
-        //if (event.which == 40) {
-            //$(this).val() = "a"
-        //}
-        $("#myOutput").html("Key: " + event.which);
+    $("#levelWidth").keydown(function(event){
+        if (event.which == 40) {
+            $("#levelWidth").val(stepNumber($("#levelWidth").val(), -1, 999, 25));
+        } else if (event.which == 38) {
+            $("#levelWidth").val(stepNumber($("#levelWidth").val(), 1, 999, 25));
+        }
+    });
+    $("#levelHeight").keydown(function(event){
+        if (event.which == 40) {
+            $("#levelHeight").val(stepNumber($("#levelHeight").val(), -1, 999, 17));
+        } else if (event.which == 38) {
+            $("#levelHeight").val(stepNumber($("#levelHeight").val(), 1, 999, 17));
+        }
+    });
+    $("#jumpHeightValue").keydown(function(event){
+        if (event.which == 40) {
+            $("#jumpHeightValue").val(stepNumber($("#jumpHeightValue").val(), -1, 99, 1));
+        } else if (event.which == 38) {
+            $("#jumpHeightValue").val(stepNumber($("#jumpHeightValue").val(), 1, 99, 1));
+        }
+    });
+    $("#gravityValue").keydown(function(event){
+        var myResult;
+        if (event.which == 40) {
+            myResult = stepNumber($("#gravityValue").val(), -0.1, 1, 0.1);
+            $("#gravityValue").val(myResult.toPrecision(1));
+        } else if (event.which == 38) {
+            myResult = stepNumber($("#gravityValue").val(), 0.1, 1, 0.1);
+            $("#gravityValue").val(myResult.toPrecision(1));
+        }
+    });
+    $("#pageValue").keydown(function(event){
+        if (event.which == 40) {
+            $("#pageValue").val(stepNumber($("#pageValue").val(), -1, 999, 1));
+        } else if (event.which == 38) {
+            $("#pageValue").val(stepNumber($("#pageValue").val(), 1, 999, 1));
+        }
     });
     
     $("#generateLevelCode").click(function(event){
         var myLevelCode, myLand, myBackground, myHeight, myWidth, myLevelName, myPayload;
         myLevelCode = $("#levelWidth").val() + "x" + $("#levelHeight").val() + "~";
         myLevelName = myURLEncode($("#levelName").val()); 
-        myPayload = ""
-        if ($("#levelStyle").val() == "grass"){
+        myPayload = "";
+        if ($("#levelStyle").val() == "grass"){ //Determine style for flatland
             myLand = "2K2M";
             myBackground = "~1~1~";
         } else if ($("#levelStyle").val() == "cave"){
@@ -77,19 +134,48 @@ $(function(){
             myBackground = "~10~10~";
         }
         for(i = 0; i < $("#levelWidth").val(); i++) {
-            myLevelCode += "0*" + ($("#levelHeight").val() - 2) + "*" + myLand
+            myLevelCode += "0*" + ($("#levelHeight").val() - 2) + "*" + myLand;
         }
+        
         if ($("#enableLDE").is(":checked")) {
             myPayload = myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/LDE.swf">');
+        } else {
+            if ($("#enableJumpHeight").is(":checked")) {
+                myPayload = myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/jumpHeight.swf">');
+                myPayload += myURLEncode("<jumpHeight:" + $("#jumpHeightValue").val() + ">");
+            }
+            if ($("#enableGravity").is(":checked")) {
+                myPayload += myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/Gravity.swf">');
+                myPayload += myURLEncode("<gravity:" + $("#gravityValue").val() + ">");
+            }
+            if ($("#enablePage").is(":checked")) {
+                myPayload += myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/Page.swf">');
+                myPayload += myURLEncode("<page:" + $("#pageValue").val() + ">");
+            }
+            if ($("#enableRespawning").is(":checked")) {
+                myPayload += myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/Respawning.swf">');
+            }
+            if ($("#enableStringData").is(":checked")) {
+                myPayload += myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/stringData.swf">');
+            }
+            if ($("#enableWiiMode").is(":checked")) {
+                myPayload += myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/Wii_Mode.swf">');
+            }
+            if ($("#enableChaosEdition").is(":checked")) {
+                myPayload += myURLEncode('<img src="https://raw.githubusercontent.com/Runouw-Modders/SM63-Mods/blob/master/public/SM63_Chaos_Edition.swf">');
+            }
         }
         
         myHeight = ($("#levelHeight").val() * 32) - 128;
         myWidth = ($("#levelWidth").val() * 32) - 128;
         myLevelCode += "~1,0," + myHeight + ",0,0,Right|"; //Mario
-        myLevelCode += "6," + myWidth + "," + myHeight; // + "|" //Shine Sprite
-        //myLevelCode += "73,48," & (myHeight + 32) & "," & mySignText & ","; //Sign with selected options
-        myLevelCode += myBackground + myLevelName + myPayload;
-        
+        myLevelCode += "6," + myWidth + "," + myHeight; //Shine Sprite
+        if ($("#payloadLocation").val() == "title") {
+            myLevelCode += myBackground + myLevelName + myPayload; //Load it in the title
+        } else {
+            myLevelCode += "|73,48," + (myHeight + 32) + "," + myPayload + ","; //Sign with selected options
+            myLevelCode += myBackground + myLevelName;
+        }
         $("#myOutput").html(myLevelCode);
     });
     
