e7a55003c6a35f867db3210a82492c38fd138b6b
Jhynjhiruu <cub3r12@gmail.com>
2017-12-26 11:22:25 +0000

    Update runouw_script.js

--- docs/runouw_script.js
+++ docs/runouw_script.js
@@ -1,5 +1,5 @@
 $(function(){
-    var assemblerVersionNumber = "0.7.7a";
+    var assemblerVersionNumber = "0.8.1a";
     $("title").append(" - Version " + assemblerVersionNumber);
     $(".versionNumberText").html(assemblerVersionNumber);
     $(".lastModifiedText").html(document.lastModified);
